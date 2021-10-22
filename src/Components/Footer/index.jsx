e17fbbbe196b0f3aaa8ab83fe05c402a12118d9a
Martinfzz <forget.l.martin@gmail.com>
2021-10-22 10:47:50 +0200

    Put text on footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="w-full h-20 bg-white static bottom-0 flex items-center justify-center">
+    <p className="text-red-600">&#169; Martin Forget 2021</p>
+  </footer>
+);
+
+export default Footer;
