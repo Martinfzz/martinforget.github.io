c8c1d542c7081dff138ca0da6693e3d594daed07
Martinfzz <forget.l.martin@gmail.com>
2021-10-25 09:04:08 +0200

    Add image to hero

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
+// import Profile_picture from "src/assets/images/profile_picture.jpg";
+import Profilepicture from '../../assets/images/profile_picture.jpg';
 
 const Hero = () => (
-  <div className="bg-red-500 py-48 flex justify-center text-center transition duration-300 ease-in-out">
-    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-      <h2 className=" tracking-tight text-white ">
-        <p className="text-3xl sm:text-4xl block font-bold">Martin Forget</p>
-        <p className="text-xl sm:text-2xl block">I&apos;m a Full Stack Developper. I&apos;ll create, recreate the website of your dreams</p>
-      </h2>
-      <div className="mt-14">
-        <a
-          href="#a"
-          className="items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-white hover:text-red-600 transition duration-500"
+  <div className="relative bg-white overflow-hidden">
+    <div className="max-w-7xl mx-auto">
+      <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <svg
+          className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+          fill="currentColor"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+          aria-hidden="true"
         >
-          CHECK MY WORK
-        </a>
+          <polygon points="50,0 100,0 50,100 0,100" />
+        </svg>
+
+        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+          {' '}
+        </div>
+
+        <main className="mx-auto max-w-7xl px-4 sm:mt-8 sm:px-6 md:mt-10 lg:mt-16 lg:px-8 xl:mt-24">
+          <div className="sm:text-center lg:text-left">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <span className="block xl:inline">Data to enrich your</span>
+              {' '}
+              <span className="block text-indigo-600 xl:inline">online business</span>
+            </h1>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+              {' '}
+              cupidatat commodo.
+              {' '}
+              Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+            </p>
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                <a
+                  href="#a"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                >
+                  Get started
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
+    </div>
+    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <img
+        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        src={Profilepicture}
+        alt="Martin Forget in the wood"
+      />
     </div>
   </div>
 );
